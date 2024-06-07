99202e7f661113e09228296d60d3b7508e2c264f
Riku-Takata <riku_takata@icloud.com>
2024-06-07 15:37:21 +0900

    second  commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import "@/styles/globals.css";
+import '@/styles/top2.css'
+import '@/styles/globals.css'
+import '@/styles/hannbaga.css'
+import '@/styles/buyer.css'
+import '@/styles/List.css'
+import '@/styles/allveg.css'
+import '@/styles/kiyaku.css'
+import '@/styles/company.css'
+import '@/styles/otoiawase.css'
+import '@/styles/akusesu.css'
+import '@/styles/question.css'
+import '@/styles/selljudge.css'
+import '@/styles/detail.css'
+import '@/styles/cart.css'
+
+
+import Header from'./header'
+import Info from'./info'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>  
+    <Header/>
+    <Component {...pageProps} />
+    <Info/>
+  </>
+  )
 }
